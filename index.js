28f67a791a56bc818c8268177598c47a06ff8320
Briana Baker <brianaclairbaker@gmail.com>
2017-11-07 17:24:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,34 +2,42 @@ const app = "I don't do much."
 
 var kittens=['Milo', 'Otis', 'Garfield'];
 
+function kittens(){
+  return kittens;
+}
+
 function destructivelyAppendKitten(name){
-  return kittens.push(name)
+  kittens.push(name);
+  return kittens;
 }
 
 function destructivelyPrependKitten(name){
-  return kittens.unshift(name)
+  kittens.unshift(name);
+  return kittens;
 }
 
-function destructivelyRemoveLastKitten(){
-  return kittens.pop()
+function destructivelyRemoveFirstKitten(name){
+  kittens.shift(name);
+  return kittens;
 }
 
-function destructivelyRemoveFirstKitten(){
-  return kittens.shift()
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
+  return kittens;
 }
 
 function appendKitten(name){
-  return [...kittens, name]
+  return [...kittens, name];
 }
 
 function prependKitten(name){
   return [name, ...kittens]
 }
 
-function removeLastKitten(){
-  return kittens.slice(0, kittens.length - 1)
+function removeFirstKitten(){
+  return kittens.slice(1);
 }
 
-function removeFirstKitten(){
-  return kittens.slice(1)
+function removeLastKitten(){
+  return kittens.slice(0, kittens.length - 1);
 }
